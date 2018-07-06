09a369d635e40e89353c8c79561a31ff7a0b2933
huanghuai <huanghuai@smartbi.com.cn>
2018-07-06 15:12:40 +0800

    update apple password

---
+++ autoCommit.bat
@@ -0,0 +1,18 @@
+﻿echo off
+cd /d %~dp0
+
+git add .
+git pull
+
+:begin
+set input=
+set /p input=please input you commit message:
+
+echo
+echo your input commit message is：%input%
+
+
+git commit -m %input%
+git push
+
+pause

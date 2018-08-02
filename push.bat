9cb2c4e3b1db15d7a53a2b4cdfe56a40b671e3e6
huanghuai <huanghuai@smartbi.com.cn>
2018-08-02 14:03:32 +0800

    1

---
+++ push.bat
@@ -0,0 +1,16 @@
+﻿echo off
+cd /d %~dp0
+
+git add .
+:begin
+set input=
+set /p input=please input you commit message:
+
+echo
+echo your input commit message is：%input%
+
+
+git commit -m "%input%"
+git push
+
+pause
